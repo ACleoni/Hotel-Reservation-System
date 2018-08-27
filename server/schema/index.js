99cea8332ed0a88cca1c0d40c44bd79f3d2002ee
Alexander Cleoni <alexander.cleoni@gmail.com>
2018-08-27 13:33:11 -0400

    Sets up index.js for graphql schema which includes mutations and queryTypes

---
+++ server/schema/index.js
@@ -0,0 +1,22 @@
+const
+{
+    GraphQLSchema,
+    GraphQLObjectType
+} = require('graphql');
+const queries = require('./queries');
+const mutations = require('./mutations');
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: () => queries
+});
+
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: () => mutations
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+    mutation: Mutation
+})
